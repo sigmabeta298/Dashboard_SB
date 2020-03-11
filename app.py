9e3f45bf706fc015193634a68fe25ce06571711d
Syamanthaka B <7438298+sigmabeta298@users.noreply.github.com>
2020-03-11 09:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
+server.secret_key = os.environ.get('secret_key', 'secret')
 app.config.suppress_callback_exceptions = True
