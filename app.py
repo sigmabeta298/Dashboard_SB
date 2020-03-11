670ffb5e31c32029b563afea30a4d11e67b3e444
Syamanthaka B <7438298+sigmabeta298@users.noreply.github.com>
2020-03-11 10:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ import layouts as lyt
 
 import callbacks
 
-server = app.server
+server = index.server
 server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(name = __name__, server = server)
 app.config.supress_callback_exceptions = True
-
-app.layout = html.Div([
-    
-    lyt.main_page
-])
-
 
 app.config.suppress_callback_exceptions = True
 if __name__ == '__main__':
